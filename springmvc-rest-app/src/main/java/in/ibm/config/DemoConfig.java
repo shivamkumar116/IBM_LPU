3ac0cd3b78ccf074b651c06a0db57cc2c35e16cf
shivam <shivamkumar116@gmail.com>
2020-04-21 15:32:14 +0530

    Added View Resolver

--- springmvc-rest-app/src/main/java/in/ibm/config/DemoConfig.java
+++ springmvc-rest-app/src/main/java/in/ibm/config/DemoConfig.java
@@ -1,13 +1,34 @@
 package in.ibm.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "in.ibm")
 public class DemoConfig implements WebMvcConfigurer {
+
+	@Bean
+	public ViewResolver viewResolver() {
+
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
 
 }
