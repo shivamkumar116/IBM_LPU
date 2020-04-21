c541382f9b271ac70f058403ddf8f78676511a76
shivam <shivamkumar116@gmail.com>
2020-04-21 12:36:00 +0530

    JACKSON json to object and vice-versa

--- jackson-databind-json-pojo/src/main/java/in/ibm/jackson_databind_json_pojo/App.java
+++ jackson-databind-json-pojo/src/main/java/in/ibm/jackson_databind_json_pojo/App.java
@@ -3,6 +3,7 @@ package in.ibm.jackson_databind_json_pojo;
 import java.io.File;
 import java.io.IOException;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -29,6 +30,34 @@ public class App {
 			System.out.println("City = " + tempAddress.getCity());
 
 		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// converting to json
+		Customer theCustomer = new Customer();
+		theCustomer.setId(2);
+		theCustomer.setFirstName("Anil");
+		theCustomer.setLastName("Kuar");
+		String[] lang = { "Hindi", "English" };
+		theCustomer.setLanguages(lang);
+		Address theAddress = new Address();
+		theAddress.setStreet("01");
+		theAddress.setCountry("IN");
+		theAddress.setCity("PLP");
+		theAddress.setState("HP");
+		theAddress.setZip("176059");
+		theCustomer.setAddress(theAddress);
+		ObjectMapper mapper= new ObjectMapper();
+		try {
+			mapper.writeValue(new File("Data/Customer.json"), theCustomer);
+		} catch (JsonGenerationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (JsonMappingException e) {
