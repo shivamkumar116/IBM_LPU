d021b9013430a6fcc7e8a05c03ec62cc97c2ebcd
shivam <shivamkumar116@gmail.com>
2020-04-23 10:54:47 +0530

    Hibernate One to one Springboot

---
+++ Springboot-Instructor-hibernate/src/main/java/in/ibm/demo/SpringbootInstructorHibernateApplication.java
@@ -0,0 +1,13 @@
+package in.ibm.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInstructorHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInstructorHibernateApplication.class, args);
+	}
+
+}
