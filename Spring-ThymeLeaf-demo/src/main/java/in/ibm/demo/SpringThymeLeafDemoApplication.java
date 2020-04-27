9a91e4bf66a10c169ca6820e395c4e6f0d3162bf
shivam <shivamkumar116@gmail.com>
2020-04-27 10:46:20 +0530

    Thymeleaf demo

---
+++ Spring-ThymeLeaf-demo/src/main/java/in/ibm/demo/SpringThymeLeafDemoApplication.java
@@ -0,0 +1,13 @@
+package in.ibm.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeLeafDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymeLeafDemoApplication.class, args);
+	}
+
+}
---
+++ Spring-ThymeLeaf-demo/src/main/java/in/ibm/demo/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package in.ibm.demo.controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	
+	
+	@RequestMapping("/")
+	public String hello(Model model) {
+		model.addAttribute("theTime",new Date());
+		return "Index";
+	}
+
+}
