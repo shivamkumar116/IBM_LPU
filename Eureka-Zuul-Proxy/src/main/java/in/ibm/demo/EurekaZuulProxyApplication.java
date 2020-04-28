b4df3d2cdb556e7aed1c07044ac219c823cfcca4
shivam <shivamkumar116@gmail.com>
2020-04-28 17:18:23 +0530

    Zuul Proxy server

---
+++ Eureka-Zuul-Proxy/src/main/java/in/ibm/demo/EurekaZuulProxyApplication.java
@@ -0,0 +1,15 @@
+package in.ibm.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class EurekaZuulProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaZuulProxyApplication.class, args);
+	}
+
+}
