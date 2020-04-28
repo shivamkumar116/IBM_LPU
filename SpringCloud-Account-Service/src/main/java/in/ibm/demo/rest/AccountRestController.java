5f3d0ef5bac2f18a9109c6ddcd5512d9dd19b75c
shivam <shivamkumar116@gmail.com>
2020-04-28 17:20:17 +0530

    Added admin server url

--- SpringCloud-Account-Service/src/main/java/in/ibm/demo/rest/AccountRestController.java
+++ SpringCloud-Account-Service/src/main/java/in/ibm/demo/rest/AccountRestController.java
@@ -1,31 +1,39 @@
 package in.ibm.demo.rest;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import in.ibm.demo.entity.Account;
 import in.ibm.demo.service.AccountService;
 
 @RestController
-@RequestMapping("/api")
 public class AccountRestController {
 	
 	private AccountService accountService;
 	
+	private Logger logger;
+	
+	
 	@Autowired
 	public AccountRestController(AccountService accountService) {
 		super();
+		logger= Logger.getLogger("accountRestController");
 		this.accountService = accountService;
 	}
 	
+	@Value("${server.port}")
+	String port;
 	
 	@GetMapping("/accounts")
 	public List<Account> getAccounts(){
+		logger.info("***port is :"+port);
 		return accountService.getAccounts();
 	}
 	@GetMapping("/accounts/{customerID}")
